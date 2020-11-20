abe10e25f9ed5bbc7fce6092917a5a797d482179
Jordi Garcia <jordi@tokyoplay.com>
2020-11-20 10:21:52 -0600

    adding confirmation screen
    co-authored-by: NanciGarcia nancilizbethgarciamartinez@gmail.com
    co-authored-by: OswaltoTovar32 oswaldotovar32@gmail.com
    co-authored-by: develiasdaniel eliasdanielsanchezlopez@gmail.com

--- FlightReservation/screens/confirmationScreen.js
+++ FlightReservation/screens/confirmationScreen.js
@@ -1,14 +1,50 @@
 import React, {useState} from 'react';
-import { View, Text} from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
+import MyFlightInfo from '../components/myFlightInfo';
+import colors from '../src/colors';
 
-
-const  ConfirmationScreen = () => {
-
-    return(
-        <View>
-            <Text>Prueba del ConfirmationScreen screen</Text>
-        </View>
-    );
-}
-
+const ConfirmationScreen = () => {
+  return (
+    <View
+      style={{
+        padding: 30,
+      }}>
+      <View style={{marginTop: 250}}>
+        <MyFlightInfo />
+      </View>
+      <View
+        style={{
+          paddingHorizontal: 20,
+          marginTop: 50,
+        }}>
+        <Text style={style.titleFont}>Your requested was received</Text>
+      </View>
+      <View>
+        <TouchableOpacity style={style.nextBtnStyle}>
+          <Text style={style.nextBtnContent}>Next</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+const style = {
+  titleFont: {
+    fontSize: 35,
+    fontWeight: 'bold',
+  },
+  nextBtnStyle: {
+    width: '100%',
+    height: 50,
+    backgroundColor: colors.grayLight,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: '60%',
+  },
+  nextBtnContent: {
+    color: colors.white,
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+};
 export default ConfirmationScreen;
